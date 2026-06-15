49ce3d18e077293ac211760197b7615e396b5961
chinzozo <chinzoobatbold1@gmail.com>
2026-06-15 11:25:08 +0800

    signup screen css

---
+++ app/components/coffeelogo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Coffeelogo = () => {
+  return (
+    <div>
+        <img src="/coffee.svg" alt="long-in screen coffee logo" className='w-5 h-5'/>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-
-
 export default function Home() {
   return (
-   <div className="w-3 h-6 bg-amber-600">
+    <div className="w-30 h-6 flex justify-center items-center rounded-md bg-amber-600">
     <p>wwwwwww</p>
    </div>
-  );
+   );
 }
