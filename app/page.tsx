68b1a2f40e705aab31bd623bcbc36643ff4aa521
chinzozo <chinzoobatbold1@gmail.com>
2026-06-15 11:30:26 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="w-30 h-6 flex justify-center items-center rounded-md bg-amber-600">
     <p>wwwwwww</p>
+    <p>w</p>
    </div>
    );
 }
