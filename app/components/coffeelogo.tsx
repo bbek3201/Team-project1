2f8a6477324669a61e674a9c008271d6c6b4e97f
bbek3201 <bbek3201@gmail.com>
2026-06-17 09:32:59 +0800

    begin

--- app/components/coffeelogo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Coffeelogo = () => {
-  return (
-    <div>
-        <img src="/coffee.svg" alt="long-in screen coffee logo" className='w-5 h-5'/>
-    </div>
-  )
-}
--- app/components/mainlogo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Mainlogo = () => {
-  return (
-    <div>
-        <img src="/mainlogo.svg" alt="long-in screen logo" className='w-60 h-60'/>
-    </div>
-  )
-}
